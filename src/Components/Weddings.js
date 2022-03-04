5b046f600db00130577c64220b18a1de62002d94
ajaySankapal <90617524+ajaySankapal@users.noreply.github.com>
2022-03-04 13:06:14 +0530

    img-grid

--- src/Components/Weddings.js
+++ src/Components/Weddings.js
@@ -1,22 +1,21 @@
 import React from "react";
 // import ImageGallery from "react-image-gallery";
 import img1 from "../img/img_wedding/pro_wedding_1.jpg";
-import img2 from "../img/img_wedding/pro_wedding_2.jpg";
+import img2 from "../img/img_wedding/pro_wedding_6.jpg";
 import img3 from "../img/img_wedding/pro_wedding_3.jpg";
 import img4 from "../img/img_wedding/pro_wedding_4.jpg";
-import img5 from "../img/img_wedding/pro_wedding_5.jpg";
-import img6 from "../img/img_wedding/pro_wedding_6.jpg";
+import img5 from "../img/img_wedding/pro_wedding_7.jpg";
+import img6 from "../img/img_wedding/pro_wedding_8.jpg";
 // import SimpleImageSlider from "react-simple-image-slider";
 
 const Weddings = () => {
   const images = [
     { url: img1 },
-    { url: img2 },
+    { url: img6 },
     { url: img3 },
     { url: img4 },
+    { url: img2 },
     { url: img5 },
-    { url: img5 },
-    { url: img6 },
   ];
   // const nu = [1, 2, 3];
   return (
@@ -96,9 +95,11 @@ const Weddings = () => {
       </div> */}
       {/* <img src={img} alt="" /> */}
       {images.map((img) => {
-        <div className="img-wrap">
-          <img src={img.url} alt="" />
-        </div>;
+        return (
+          <div className="img-wrap" key={new Date().getTime()}>
+            <img src={img.url} alt="" />
+          </div>
+        );
       })}
     </div>
   );
