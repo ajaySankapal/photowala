b7dafd547220175364cbb274136859f3e1083ccc
ajaySankapal <90617524+ajaySankapal@users.noreply.github.com>
2022-10-26 13:17:11 +0530

    decreased the loading time of the web page

--- src/Components/Street.js
+++ src/Components/Street.js
@@ -1,23 +1,23 @@
-import React from "react";
-import img1 from "../img/img_street/pro_street_1.jpg";
-import img2 from "../img/img_street/pro_street_2.jpg";
-import img3 from "../img/img_street/pro_street_3.jpg";
-import img4 from "../img/img_street/pro_street_4.jpg";
-import img5 from "../img/img_street/pro_street_5.jpg";
-import img6 from "../img/img_street/pro_street_6.jpg";
+import React from 'react'
+import img1 from '../img/img_street/pro_street_1.jpg'
+import img2 from '../img/img_street/pro_street_2.jpg'
+import img3 from '../img/img_street/pro_street_3.jpg'
+import img4 from '../img/img_street/pro_street_4.jpg'
+import img5 from '../img/img_street/pro_street_5.jpg'
+import img6 from '../img/img_street/pro_street_6.jpg'
 const Street = ({ setSelectedImg }) => {
-  const images = [img1, img2, img3, img4, img5, img6];
+  const images = [img1, img2, img3, img4, img5, img6]
   return (
-    <div className="img-grid">
+    <div className='img-grid'>
       {images.map((img) => {
         return (
-          <div className="img-wrap" onClick={() => setSelectedImg(img)}>
-            <img src={img} alt="" />
+          <div className='img-wrap' onClick={() => setSelectedImg(img)}>
+            <img src={img} alt='' />
           </div>
-        );
+        )
       })}
     </div>
-  );
-};
+  )
+}
 
-export default Street;
+export default Street
